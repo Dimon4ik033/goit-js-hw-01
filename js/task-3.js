6e49a52f315a2ca6c62ff334781bb96bd8ceb8a5
Dmirty Naumenko <dimanaumenko31@gmail.com>
2024-08-25 14:11:49 +0300

    end

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function getElementWidth(content, padding, border) {
+     const contentWidth = parseFloat(content);
+     const paddingWidth = parseFloat(padding);
+     const borderWidth = parseFloat(border);
+    
+    const total = contentWidth + (paddingWidth * 2) + (borderWidth * 2);
+    return total;
+
+}
+
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
